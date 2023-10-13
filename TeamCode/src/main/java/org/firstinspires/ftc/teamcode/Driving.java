fdf1fb5a9d3767ca22599067dc8dee3adead0e3f
Sofiane-Lachab <slashab16@gmail.com>
2023-10-12 21:13:14 -0400

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving.java
@@ -62,7 +62,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-
+//hi this yo mom
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
 
 public class Driving extends LinearOpMode {
