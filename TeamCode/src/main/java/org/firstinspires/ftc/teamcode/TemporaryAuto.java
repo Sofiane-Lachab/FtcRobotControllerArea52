781be50ea3cb1f7e91149daba2bad64a6d4cc621
Sofiane-Lachab <slashab16@gmail.com>
2023-11-17 15:43:49 -0500

    We have autonomous that parks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemporaryAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemporaryAuto.java
@@ -10,55 +10,62 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class TemporaryAuto extends LinearOpMode{
 
-    DcMotor motorFrontLeft = null;
-    DcMotor motorBackLeft = null;
-    DcMotor motorFrontRight = null;
-    DcMotor motorBackRight = null;
-    double drive_power = 0.6;
+    DcMotor leftFrontDrive = null;
+    DcMotor leftBackDrive = null;
+    DcMotor rightFrontDrive = null;
+    DcMotor rightBackDrive = null;
+    double drive_power = 0.4;
 
     public void runOpMode() throws InterruptedException{
-        motorFrontLeft = hardwareMap.dcMotor.get("leftFrontMotor");
-        motorBackLeft = hardwareMap.dcMotor.get("leftBackMotor");
-        motorFrontRight = hardwareMap.dcMotor.get("rightFrontMotor");
-        motorBackRight = hardwareMap.dcMotor.get("rightBackMotor");
+        leftFrontDrive = hardwareMap.dcMotor.get("leftFrontMotor");
+        leftBackDrive = hardwareMap.dcMotor.get("leftBackMotor");
+        rightFrontDrive = hardwareMap.dcMotor.get("rightFrontMotor");
+        rightBackDrive = hardwareMap.dcMotor.get("rightBackMotor");
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
-        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
 
 
         // zero power behavior
-        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
+//         forward(drive_power);
+//         Thread.sleep(1000);
+//         strafeRight(drive_power);
+//         Thread.sleep(1000);
+//         backward(drive_power);
+//         Thread.sleep(1000);
+//         strafeLeft(drive_power);
+//         Thread.sleep(1000);
+//         turnRight(drive_power);
+//         Thread.sleep(1500);
         forward(drive_power);
-        Thread.sleep(1000);
-        strafeRight(drive_power);
-        Thread.sleep(1000);
-        backward(drive_power);
-        Thread.sleep(1000);
+        Thread.sleep(500);
         strafeLeft(drive_power);
-        Thread.sleep(1000);
-        turnRight(drive_power*3);
+        Thread.sleep(1500);
         stopDriving();
     }
 
     public void forward(double drive_power){
-        motorFrontLeft.setPower(-drive_power);
-        motorBackLeft.setPower(-drive_power);
-        motorFrontRight.setPower(-drive_power);
-        motorBackRight.setPower(-drive_power);
+        leftFrontDrive.setPower(drive_power);
+        leftBackDrive.setPower(drive_power);
+        rightFrontDrive.setPower(drive_power);
+        rightBackDrive.setPower(drive_power);
     }
     // drive backward
     public void backward(double drive_power){
-        motorFrontLeft.setPower(drive_power);
-        motorBackLeft.setPower(drive_power);
-        motorFrontRight.setPower(drive_power);
-        motorBackRight.setPower(drive_power);
+        leftFrontDrive.setPower(-drive_power);
+        leftBackDrive.setPower(-drive_power);
+        rightFrontDrive.setPower(-drive_power);
+        rightBackDrive.setPower(-drive_power);
     }
     // stop
     public void stopDriving(){
@@ -66,32 +73,32 @@ public class TemporaryAuto extends LinearOpMode{
     }
     // turn left
     public void turnLeft(double drive_power){
-        motorFrontLeft.setPower(drive_power);
-        motorBackLeft.setPower(drive_power);
-        motorFrontRight.setPower(-drive_power);
-        motorBackRight.setPower(-drive_power);
+        leftFrontDrive.setPower(-drive_power);
+        leftBackDrive.setPower(-drive_power);
+        rightFrontDrive.setPower(drive_power);
+        rightBackDrive.setPower(drive_power);
     }
 
     // turn right
     public void turnRight(double drive_power){
-        motorFrontLeft.setPower(-drive_power);
-        motorBackLeft.setPower(-drive_power);
-        motorFrontRight.setPower(drive_power);
-        motorBackRight.setPower(drive_power);
+        leftBackDrive.setPower(drive_power);
+        leftBackDrive.setPower(drive_power);
+        rightFrontDrive.setPower(-drive_power);
+        rightBackDrive.setPower(-drive_power);
     }
 
     // strafe left
     public void strafeLeft(double drive_power){
-        motorFrontLeft.setPower(drive_power);
-        motorBackLeft.setPower(-drive_power);
-        motorFrontRight.setPower(-drive_power);
-        motorBackRight.setPower(drive_power);
+        leftFrontDrive.setPower(-drive_power);
+        leftBackDrive.setPower(drive_power);
+        rightFrontDrive.setPower(drive_power);
+        rightBackDrive.setPower(-drive_power);
     }
     // strafe right
     public void strafeRight(double drive_power){
-        motorFrontLeft.setPower(-drive_power);
-        motorBackLeft.setPower(drive_power);
-        motorFrontRight.setPower(drive_power);
-        motorBackRight.setPower(-drive_power);
+        leftFrontDrive.setPower(drive_power);
+        leftBackDrive.setPower(-drive_power);
+        rightFrontDrive.setPower(-drive_power);
+        rightBackDrive.setPower(drive_power);
     }
 }
